5078d79b71af89e966431d090792fe336c3ffdc8
Woody <nz.wgkim@gmail.com>
2023-03-13 17:02:30 +1300

    from version 6

--- lib/main.dart
+++ lib/main.dart
@@ -22,15 +22,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routeInformationProvider: _router.routeInformationProvider,
-
-      /// URI String을 상태 및 Go Router에서 사용할 수
-      /// 있는 형태로 변환해 주는 함수
-      routeInformationParser: _router.routeInformationParser,
-
-      /// 위에서 변경된 값으로
-      /// 실제 어떤 route를 보여줄지 정하는 함수
-      routerDelegate: _router.routerDelegate,
+      routerConfig: _router,
     );
   }
 }
